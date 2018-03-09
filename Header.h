8e767c2180b83c58283227045c285e78d0f5e06d
Ting Li Shen <dein0522@gmail.com>
2018-03-09 03:47:59 -0500

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <string>
+using namespace std;
+class tree
+{
+private:
+public:
+	string num;
+	int zero;
+	int cul;
+	int row;
+	void setZero(int x) { zero = x; };
+	void setString(string x) { num = x; };
+	int zeroSite();
+
+};
